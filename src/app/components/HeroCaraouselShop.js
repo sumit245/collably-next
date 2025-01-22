4222e543e8edce2334739daec9fa88d6138c0706
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-01-22 22:34:06 +0530

    skeleton building

--- src/app/components/HeroCaraouselShop.js
+++ src/app/components/HeroCaraouselShop.js
@@ -1,13 +1,44 @@
-import VideoCarouselCard from './Cards/VideoCard';
+"use client";
 
-export default function HeroCarousel({ data }) {
+import { useState, useEffect } from "react";
+import VideoCarouselCard from "./Cards/VideoCard";
+import styles from "../cart/styleCart.module.css";
+
+export default function HeroCarousel() {
+  const [data, setData] = useState(null); 
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <main>
-      <VideoCarouselCard 
-        videos={data.videos} 
-        title={data.title} 
-        subtitle={data.subtitle} 
-      />
+      {isLoading ? (
+       
+        <div className={styles.skeletonContainer}>
+          <div className={styles.skeletonTitle}></div>
+          <div className={styles.skeletonSubtitle}></div>
+          <div className={styles.skeletonVideos}>
+            {Array.from({ length: 3}).map((_, index) => (
+              <div key={index} className={styles.skeletonVideo}></div>
+            ))}
+          </div>
+        </div>
+      ) : (
+
+        data ? (
+          <VideoCarouselCard
+            videos={data.videos}
+            title={data.title}
+            subtitle={data.subtitle}
+          />
+        ) : (
+          <div className={styles.noDataContainer}>
+            <p>No data available yet.</p>
+          </div>
+        )
+      )}
     </main>
   );
 }
