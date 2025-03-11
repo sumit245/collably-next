05dcfe71028366932779a44782abab12f3d526e3
dheerajyadav1468 <dheerajyadav1468@gmail.com>
2025-03-11 14:43:42 +0530

    skeleton added in herocaraousel of shop page

--- src/app/components/HeroCaraouselShop.js
+++ src/app/components/HeroCaraouselShop.js
@@ -1,19 +1,27 @@
-// heroCarousel.js
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts } from '../store/postSlice';
 import VideoCarouselCard from './Cards/VideoCard';
 import { BASE_URL } from "../services/api";
+import styles from "../cart/styleCart.module.css";
+
 export default function HeroCarousel({ data }) {
   const dispatch = useDispatch();
   const { posts, status } = useSelector((state) => state.posts);
-  
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchPosts());
     }
   }, [status, dispatch]);
+
+  useEffect(() => {
+    if (status === 'succeeded' && posts.length > 0) {
+      setIsLoading(false);
+    }
+  }, [status, posts]);
 
   const videoData = posts
     .filter(post => post.video)
@@ -25,11 +33,23 @@ export default function HeroCarousel({ data }) {
 
   return (
     <main>
-      <VideoCarouselCard 
-        videos={videoData} 
-        title={data.title} 
-        subtitle={data.subtitle} 
-      />
+      {isLoading || videoData.length === 0 ? (
+        <div className={styles.skeletonContainer}>
+          <div className={styles.skeletonTitle}></div>
+          <div className={styles.skeletonSubtitle}></div>
+          <div className={styles.skeletonVideos}>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <div key={index} className={styles.skeletonVideo}></div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <VideoCarouselCard 
+          videos={videoData} 
+          title={data.title} 
+          subtitle={data.subtitle} 
+        />
+      )}
     </main>
   );
 }
